0a33a6ff595801a877f91d195c1dc664cd0be276
ChrisWayFor <christopher.farcoz@gmail.com>
2024-04-03 11:56:51 +0200

    revolve

--- thanks.php
+++ thanks.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST['user_name'])) {
         $nameErr = "T'as pas mis le nom ! ";
     } else {
-        $name = ($_POST['user_name']);
+        $name = test_input($_POST['user_name']);
         if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
             $nameErr = "Seulement les lettres et les espaces sont acceptés ma biche ! ";
         }
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST['user_firstname'])) {
         $firstnameErr = "T'as pas mis le prénom ! ";
     } else {
-        $firstname = ($_POST['user_firstname']);
+        $firstname = test_input($_POST['user_firstname']);
         if (!preg_match("/^[a-zA-Z-' ]*$/", $firstname)) {
             $firstnameErr = "Seulement les lettres et les espaces sont acceptés ma biche ! ";
         }
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST['user_email'])) {
         $emailErr = "Ton E-mail est requis";
     } else {
-        $email = ($_POST['user_email']);
+        $email = test_input($_POST['user_email']);
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Format du mail non valide !";
         }
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST['user_phone_number'])) {
         $phoneErr = "Ton numéro de téléphone est requis";
     } else {
-        $phone = ($_POST['user_phone_number']);
+        $phone = test_input($_POST['user_phone_number']);
         if (!preg_match("/^[0-9]{10}$/", $phone)) {
             $phoneErr = "Ton téléphone ne fonctionne pas";
         }
@@ -56,20 +56,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST['user_subject'])) {
         $subjectErr = 'Le sujet du message est requis';
     } else {
-        $subject = ($_POST['user_subject']);
+        $subject = test_input($_POST['user_subject']);
     }
 
     if (empty($_POST['user_message'])) {
         $messageErr = 'Un message est requis, parle moi enfin !';
     } else {
-        $message = ($_POST['user_message']);
+        $message = test_input($_POST['user_message']);
     }
 
     if (empty($nameErr) && empty($firstnameErr) && empty($emailErr) && empty($phoneErr) && empty($subjectErr) && empty($messageErr)) {
         echo "Merci $firstname $name de nous avoir contacté à propos de \"$subject\"." . BR . BR;
         echo "Un de nos conseillers vous contactera soit à l'adresse $email ou par téléphone au $phone dans les plus brefs délais pour traiter votre demande :" . BR . BR;
         echo "$message\n";
+    } else {
+        echo $nameErr . $firstnameErr . $emailErr . $phoneErr . $subjectErr . $messageErr;
     }
-    else { echo $nameErr . $firstnameErr . $emailErr . $phoneErr . $subjectErr . $messageErr; }
-
 }
